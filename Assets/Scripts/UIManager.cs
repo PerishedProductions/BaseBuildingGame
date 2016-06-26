b0847409a30e57707384b85e6d15476775e8fcfd
THEXG4MING <emilsolo@sol.dk>
2016-06-26 22:54:49 +0200

    Added tile sprites & dirt sprite

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -30,6 +30,6 @@ public class UIManager : MonoBehaviour {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
-        fpsCounter.text = string.Format("{1:0.} fps", msec, fps);
+        fpsCounter.text = string.Format("{1:0.} FPS", msec, fps);
     }
 }
