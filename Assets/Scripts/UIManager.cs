5eef36305c4481c187bef19041bdc3a464c8e486
THEXG4MING <emilsolo@sol.dk>
2016-06-26 21:52:34 +0200

    Added fpsCounter

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
+
+    private float deltaTime = 0;
+
+    public Text fpsCounter;
 
     public MouseManager mouseManager;
 
@@ -20,4 +25,11 @@ public class UIManager : MonoBehaviour {
         mouseManager.StartBuild(Tile.TileType.Wall);
     }
 	
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        fpsCounter.text = string.Format("{1:0.} fps", msec, fps);
+    }
 }
